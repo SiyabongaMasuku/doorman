f62b91e427cffbe6ef6d1dfd82c3b865938485e1
Siyabonga Masuku <matupa.masuku@gmail.com>
2019-11-21 13:05:43 +0200

    password check method, and while
    Ask for the password util the valid password is entered. Valid according to the defined rules.

--- password.java
+++ password.java
@@ -2,42 +2,70 @@ import java.util.Scanner;
 
 public class password {
 
-	public static void main(String[] args) {
-		
+	public static boolean pCheck() {
+
+		boolean status;// = false;
+
 		Scanner in = new Scanner(System.in);
-		String sPass;
-		int sml=0;//to hold small letters count
-		int cap=0;//to hold capital letters count
-		int dig=0;//to hold digit count
+		int sml = 0;// to hold small letters count
+		int cap = 0;// to hold capital letters count
+		int dig = 0;// to hold digit count
 		System.out.println("Enter pass: ");
+		String sPass, cleanbuffer;
 		sPass = in.nextLine();
-		in.close();
-		int pl;//to hold the length of the string password
+//		while (in.hasNext()) {
+//			cleanbuffer = in.nextLine();
+//			System.out.println("******");
+//		}
+		
+		
+//		in.close();
+		int pl;// password length
 		pl = sPass.length();
-		if(pl > 6 || pl < 6) {
-			System.out.println("INVALID");			
-		}
-		else {
-			for (int i=0; i<pl;i++) {
+		if (pl > 6 || pl < 6) {
+//			System.out.println("INVALID");
+			status = false;
+		} else { // else 1
+			for (int i = 0; i < pl; i++) {
 				char letter = sPass.charAt(i);
-				if(Character.isLowerCase(letter)) {
+				if (Character.isLowerCase(letter)) {
 					sml++;
+
 				}
-				if(Character.isUpperCase(letter)) {
+				if (Character.isUpperCase(letter)) {
 					cap++;
+
 				}
-				if(Character.isDigit(letter)) {
+				if (Character.isDigit(letter)) {
 					dig++;
+
 				}
-				
-			} //close for loop
-			if(cap ==2 && sml == 2 && dig==2) {
-				System.out.println("VALID");
-			}
-			else {
-				System.out.println("INVALID");
-			}
-		}
+
+			} // close for loop
+			if (cap == 2 && sml == 2 && dig == 2) {
+//				System.out.println("VALID");
+				status = true;
+
+			} else {// else 2
+//				System.out.println("INVALID");
+				status = false;
+			} // close else 2
+		} // close else 1
+
+		return status;
+
 	}
 
-}
+	public static void main(String[] args) {
+
+		while (!pCheck()) {
+			System.out.println("Please try again...");
+
+		}
+		System.out.println("Password created. Thank you.");
+		
+		
+
+}// main
+
+}// class
